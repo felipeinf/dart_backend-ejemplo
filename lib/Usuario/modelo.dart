939eb02c11c62142d240ad2062d67688607df1d7
Felipe mac <fconcha.almeida@gmail.com>
2019-09-26 18:09:05 -0300

    feat: se creo la  entidad usuario y su modelo de datos

---
+++ lib/Usuario/modelo.dart
@@ -0,0 +1,29 @@
+class Usuario {
+
+  Usuario(String id, String nombre) {
+    _id = id;
+    _nombre = nombre;
+  }
+
+  String _id;
+  String _nombre;
+  DateTime _creado;
+  DateTime _modificado;
+
+  Map<String, dynamic> toJson() => {
+    '_id': _id,
+    'nombre': _nombre,
+    'creado': _creado.toIso8601String(),
+    'modificado': _modificado.toIso8601String()
+  };
+
+  String get id => _id;
+
+  set nombre(String nombre) {
+    _nombre = nombre;
+  }
+
+  set modificado(String descripcion) {
+    _modificado = DateTime.now();
+  }
+}
