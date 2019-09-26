e041a83db6ddf743747ad9018fdb91f8df1e8e30
Felipe mac <fconcha.almeida@gmail.com>
2019-09-26 18:35:39 -0300

    refactor: se separo el puerto en una constante

--- main.dart
+++ main.dart
@@ -1,8 +1,9 @@
 import 'package:api_tareas/api_tareas.dart';
 
 Future main() async {
+  const puerto = 8888;
   final app = Application<RouterApi>()
-    ..options.port = 8888;
+    ..options.port = puerto;
 
   final count = Platform.numberOfProcessors ~/ 2;
   await app.start(numberOfInstances: count > 0 ? count : 1);
